bcb44466220eab2837b8c26e35e0f112170d4b12
Thor Nissen <thorjnissen@gmail.com>
2021-08-18 13:11:45 +0200

    initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import VueDawaAutocomplete from '@/vue-dawa-autocomplete.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueDawaAutocomplete
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-dawa-autocomplete />
+  </div>
+</template>
